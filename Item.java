ce311d96b839610a039ce84046aa955c8cae0e72
Yash <yashgandhi2020@gmail.com>
2021-09-02 16:22:59 +0530

    Did all changes as per mentioned

--- Item.java
+++ Item.java
@@ -1,13 +1,27 @@
-public class Item {
+public final class Item {
 
-    String barcode;
-    String itemName;
-    double price;
+    private String barcode;
+    private String itemName;
+    private double price;
 
-    Item(String barcode, String itemName, double price){
+    Item(String barcode, String itemName, double price) {
         this.barcode = barcode;
         this.itemName = itemName;
         this.price = price;
+    }
+
+    // getter methods
+
+    String getBarcode() {
+        return this.barcode;
+    }
+
+    String getItemName() {
+        return this.itemName;
+    }
+
+    double getPrice() {
+        return this.price;
     }
 
 }
--- ShoppingCart.java
+++ ShoppingCart.java
@@ -2,77 +2,66 @@ import java.util.*;
 
 class ShoppingCart {
 
-    HashMap<String, Item> map = new HashMap<>();
-    ArrayList<Item> cart = new ArrayList<>();
-    HashMap<String, Integer> numberOfItem = new HashMap<>();
-    double totalBill = 0;
+    private HashMap<String, Item> productMap = new HashMap<>();
+    private HashMap<String, Integer> itemsInCart = new HashMap<>();
 
     ShoppingCart() {
-        map.put("01001", new Item("01001", "Banana", 0.99));
-        map.put("01002", new Item("01002", "Apple", 1.30));
-        map.put("01003", new Item("01003", "Carrot", 1.66));
+        productMap.put("01001", new Item("01001", "Banana", 0.99));
+        productMap.put("01002", new Item("01002", "Apple", 1.30));
+        productMap.put("01003", new Item("01003", "Carrot", 1.66));
     }
 
     void addItemToLIst(String barcode) {
 
-        if (numberOfItem.containsKey(barcode)) {
-            numberOfItem.replace(barcode, numberOfItem.get(barcode) + 1);
+        if (itemsInCart.containsKey(barcode)) {
+            itemsInCart.replace(barcode, itemsInCart.get(barcode) + 1);
         } else {
-            numberOfItem.put(barcode, 1);
-            Item item = map.get(barcode);
-            cart.add(item);
+            itemsInCart.put(barcode, 1);
         }
-        totalBill += map.get(barcode).price;
     }
 
     // it will remove only one item, example if there are 3 banana then it will
     // remove 1 and now there are only 2 banana now.
     void removeItemFromList(String barcode) {
-        if (numberOfItem.get(barcode) > 1) {
-            numberOfItem.replace(barcode, numberOfItem.get(barcode) - 1);
+        if (itemsInCart.get(barcode) > 1) {
+            itemsInCart.replace(barcode, itemsInCart.get(barcode) - 1);
         } else {
-            if (numberOfItem.get(barcode) == 1) {
-                numberOfItem.remove(barcode);
-                for (int i = 0; i < cart.size(); i++) {
-                    if (cart.get(i).barcode == barcode) {
-                        cart.remove(i);
-                        break;
-                    }
-                }
+            if (itemsInCart.get(barcode) == 1) {
+                itemsInCart.remove(barcode);
             }
         }
-        totalBill -= map.get(barcode).price;
     }
 
     // it will delete item from list like if there are 2 banana then it will delete
     // all 3 banana from it.
     void deleteItemFromList(String barcode) {
-        if (numberOfItem.containsKey(barcode)) {
-            totalBill -= (numberOfItem.get(barcode) * map.get(barcode).price);
-            numberOfItem.remove(barcode);
-            for (int i = 0; i < cart.size(); i++) {
-                if (cart.get(i).barcode == barcode) {
-                    cart.remove(i);
-                    break;
-                }
-            }
+        if (itemsInCart.containsKey(barcode)) {
+            itemsInCart.remove(barcode);
         }
     }
 
-    void printCart(){
-        numberOfItem.forEach((key, value) -> {
-            System.out.println(value + " x " + map.get(key).itemName + " @ "
-                    + map.get(key).price + " = " + (value * map.get(key).price));
-        });
-        System.out.println("Total = " + totalBill);
-        System.out.println("\n");
-        System.out.println("Barcode\t Description\t Price\t");
-        System.out.println("--------------------------------");
-        for (int i = 0; i < numberOfItem.size(); i++) {
-
-            Item tempItem = cart.get(i);
-            System.out.println(tempItem.barcode + "\t" + tempItem.itemName + "\t\t" + tempItem.price);
+    double getTotalBill() {
+        double totalBill = 0;
+        for (Map.Entry<String, Integer> eachItem : itemsInCart.entrySet()) {
+            totalBill += (productMap.get(eachItem.getKey()).getPrice() * eachItem.getValue());
         }
+        return totalBill;
+    }
+
+    void printCart() {
+        itemsInCart.forEach((key, value) -> {
+            System.out.println(value + " x " + productMap.get(key).getItemName() + " @ " + productMap.get(key).getPrice() + " = "
+                    + (value * productMap.get(key).getPrice()));
+        });
+        System.out.println("Total = " + getTotalBill());
+        System.out.println("\nBarcode\t Description\t Price\t");
+        System.out.println("--------------------------------");
+        for (int i = 0; i < itemsInCart.size(); i++) {
+
+        }
+        productMap.forEach((key, value) -> {
+            System.out.println(key + "\t" + value.getItemName() + "\t\t" + value.getPrice());
+        });
     }
 
     public static void main(String args[]) {
@@ -87,6 +76,5 @@ class ShoppingCart {
         shoppingCart.addItemToLIst("01003");
 
         shoppingCart.printCart();
-
     }
 }
